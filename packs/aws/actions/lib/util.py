53ccd97698fba123f69b6cd98e3f840a382dcba6
amith jayasekara <amith.jayasekara@pearson.com>
2016-12-14 14:52:12 +0530

    fixing flake8 issues

--- packs/aws/actions/lib/util.py
+++ packs/aws/actions/lib/util.py
@@ -1,39 +1,37 @@
-import boto.ec2.elb
 from boto.ec2.elb import HealthCheck
 
-def get_listners(listener_string):
-        listeners = listener_string.split('#')
-        
-        if len(listeners) == 0:
-                listeners = listener_string
 
+def get_listners(listener_string):
+    listeners = listener_string.split('#')
+
+    if len(listeners) == 0:
+        listeners = listener_string
         listener_list = []
 
         for i in range(len(listeners)):
-                values = listeners[i].split(',')
-                tup = (int(values[0]),int(values[1]),values[2])
+            values = listeners[i].split(',')
+            tup = (int(values[0]), int(values[1]), values[2])
 
-                listener_list.append(tup)
+            listener_list.append(tup)
 
-        return listener_list
+    return listener_list
+
 
 def populate_elb_health_check(kwargs):
 
-        healthy_threshold = kwargs['healthy_threshold']
-        interval = kwargs['interval']
-        unhealthy_threshold = kwargs['unhealthy_threshold']
-        target = kwargs['target']
+    healthy_threshold = kwargs['healthy_threshold']
+    interval = kwargs['interval']
+    unhealthy_threshold = kwargs['unhealthy_threshold']
+    target_value = kwargs['target']
 
-        health_check = HealthCheck(
-                        interval=int(healthy_threshold),
-                        healthy_threshold=int(interval),
-                        unhealthy_threshold=int(unhealthy_threshold),
-                        target='TCP:22')
-        
+    health_check = HealthCheck(interval=int(healthy_threshold),
+        healthy_threshold=int(interval),
+        unhealthy_threshold=int(unhealthy_threshold),
+        target=target_value)
 
-        del kwargs['healthy_threshold']
-        del kwargs['interval']
-        del kwargs['unhealthy_threshold']
-        del kwargs['target']
+    del kwargs['healthy_threshold']
+    del kwargs['interval']
+    del kwargs['unhealthy_threshold']
+    del kwargs['target']
 
-        kwargs['health_check']=health_check
+    kwargs['health_check'] = health_check
--- packs/aws/actions/run.py
+++ packs/aws/actions/run.py
@@ -1,6 +1,7 @@
 from lib import action
 from lib import util
 
+
 class ActionManager(action.BaseAction):
 
     def run(self, **kwargs):
@@ -13,10 +14,10 @@ class ActionManager(action.BaseAction):
         if action == 'create_tags':
             kwargs['tags'] = self.split_tags(kwargs['tags'])
         if action == 'create_load_balancer' or action == 'create_load_balancer_listeners':
-            if kwargs['listeners']  is not None:
-                kwargs['listeners'] = util.get_listners(kwargs['listeners'])  
+            if kwargs['listeners'] is not None:
+                kwargs['listeners'] = util.get_listners(kwargs['listeners'])
         if action == 'configure_health_check':
-            util.populate_elb_health_check(kwargs)                
+            util.populate_elb_health_check(kwargs)
         if action in ('add_a', 'update_a'):
             kwargs['value'] = kwargs['value'].split(',')
         if 'cls' in kwargs.keys():

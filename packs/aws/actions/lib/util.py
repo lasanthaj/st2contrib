9382dc3a3e52c9c02963ad78c47201781f809cb4
pavithraMG <pavithra.gunawardena@pearson.com>
2016-12-15 12:33:54 +0530

    Update util.py
    Changed for loop order.

--- packs/aws/actions/lib/util.py
+++ packs/aws/actions/lib/util.py
@@ -8,11 +8,11 @@ def get_listners(listener_string):
         listeners = listener_string
         listener_list = []
 
-        for i in range(len(listeners)):
-            values = listeners[i].split(',')
-            tup = (int(values[0]), int(values[1]), values[2])
+    for i in range(len(listeners)):
+        values = listeners[i].split(',')
+        tup = (int(values[0]), int(values[1]), values[2])
 
-            listener_list.append(tup)
+        listener_list.append(tup)
 
     return listener_list
 
